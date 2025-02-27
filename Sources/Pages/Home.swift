c8fa93c23d7d1024a66ba0b84a4e8b5f95069c11
Justin Purnell <jpurnell@users.noreply.github.com>
2025-02-27 08:47:19 -0500

    add core files

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -5,7 +5,10 @@ struct Home: StaticLayout {
     var title = "Home"
 
     var body: some HTML {
-        Text("Tawana Cook Purnell - For District 1")
-            .font(.title1)
+		Text("Vote")
+        Text("Tawana Cook Purnell").font(.title1).fontWeight(.bold)
+		Text("For District 1").font(.title2).fontWeight(.bold)
+		Text("PITTSBURGH PUBLIC SCHOOLS BOARD OF DIRECTORS").font(.title2)
+		
     }
 }
