d4c1f1caf4c0a16b6ab9b658cbae09eab495ae99
Justin Purnell <jpurnell@users.noreply.github.com>
2025-02-27 08:52:35 -0500

    update label for accessibility

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -9,7 +9,6 @@ struct Home: StaticLayout {
 //        Text("Tawana Cook Purnell").font(.title1).fontWeight(.bold)
 //		Text("For District 1").font(.title2).fontWeight(.bold)
 //		Text("PITTSBURGH PUBLIC SCHOOLS BOARD OF DIRECTORS").font(.title2)
-		Image("/images/flyer.png")
-		
+		Image("/images/flyer.png", description: "Vote Tawana Cook Purnell for District 1 School Board on May 26")
     }
 }
