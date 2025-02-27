00e6c7e78d6eb2e70016359092b20786e9d48387
Justin Purnell <jpurnell@users.noreply.github.com>
2025-02-27 08:50:52 -0500

    post flyer

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -5,10 +5,11 @@ struct Home: StaticLayout {
     var title = "Home"
 
     var body: some HTML {
-		Text("Vote")
-        Text("Tawana Cook Purnell").font(.title1).fontWeight(.bold)
-		Text("For District 1").font(.title2).fontWeight(.bold)
-		Text("PITTSBURGH PUBLIC SCHOOLS BOARD OF DIRECTORS").font(.title2)
+//		Text("Vote")
+//        Text("Tawana Cook Purnell").font(.title1).fontWeight(.bold)
+//		Text("For District 1").font(.title2).fontWeight(.bold)
+//		Text("PITTSBURGH PUBLIC SCHOOLS BOARD OF DIRECTORS").font(.title2)
+		Image("/images/flyer.png")
 		
     }
 }
